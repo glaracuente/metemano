aab68419031543d68d03c0028220efd24cb51e95
Gerry Laracuente <grlaracuente@gmail.com>
2020-05-16 01:15:29 -0400

    put pics into squared off divs

--- assets/js/metemano.js
+++ assets/js/metemano.js
@@ -4,7 +4,7 @@ $(document).ready(function () {
 		var tags = ["bathroom", "customWork", "kitchen"];
 		for (var t = 0; t < tags.length; t++) {
 			for (var i = 0; i < 3; i++) {
-			var imgDiv = $("<div>").addClass("media").addClass("all").addClass(tags[t]);
+			var imgDiv = $("<div>").addClass("media").addClass("all").addClass(tags[t]).css({ 'height': '200px', 'width': '200px' }); ;
 			var currentHref = $("<a>").attr("href", `images/portfolio/${tags[t]}/${i}.jpg`);
 			var currentImage = $("<img>").attr("src", `images/portfolio/${tags[t]}/${i}.jpg`);
 			currentHref.wrapInner(currentImage)
